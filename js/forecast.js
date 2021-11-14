cb088ecd47b60342c661ac5c5f737b126c2f61e7
RobertMaina <robertmaina254@gmail.com>
2021-11-14 19:57:09 +0300

    get accuweather api

--- js/forecast.js
+++ js/forecast.js
@@ -1,1 +1,19 @@
 // responsible for interacting with weather api 
+
+const key = ' zsjvuKvkoPAV6FqCFxPBLSyVcghkq7xp';
+
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base+query);
+    const data = await response.json()          // json method returns a promise
+    // console.log(data[0]);
+    return data[0];
+
+};
+
+getCity('nairobi')
+.then(data => console.log(data))
+.catch(err => console.log('search again'));
